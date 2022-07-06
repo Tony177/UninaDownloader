ff18d7da35041313cffbb234c947315a47998db6
Antonio Avolio <antonioavolio18@gmail.com>
2022-07-06 15:49:16 +0200

    Added Crypto Test using PyTest
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

---
+++ test/test_crypt.py
@@ -0,0 +1,34 @@
+import crypt
+import pytest
+from os import listdir, remove, chdir
+from os.path import dirname, realpath
+
+
+@pytest.fixture(scope="module")
+def setup():
+    chdir(dirname(realpath(__file__)))
+    crypt.gen_key()
+    yield
+    remove("secret.key")
+
+
+@pytest.fixture
+def tmp_file_dec():
+    with open("test.txt", 'w+') as f:
+        f.write("Test\nDone")
+        file = f.read()
+    yield file
+    remove("test.txt")
+
+def test_gen(setup):
+    assert "secret.key" in listdir()
+
+
+def test_encrypt(setup):
+    crypt.encrypt("test.dat", "Test\nDone".encode())
+    assert "test.dat" in listdir()
+
+
+def test_decrypt(tmp_file_dec, setup):
+    dec = crypt.decrypt("test.dat")
+    assert " ".join(dec) == "Test Done"
